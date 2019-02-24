bf5380b98e44dd20f953fa5e86067a2ba68e4174
wwwbrunagon <wwwbrunagon@gmail.com>
2019-02-24 20:48:03 -0300

    Delete Function and LocalStorage save action

--- mainscript.js
+++ mainscript.js
@@ -22,26 +22,56 @@ const PrintItem = () => {
     formList.innerHTML =''
     arr = JSON.parse(localStorage.getItem('rutina'))
     console.log(arr)
-
+    
     if (arr === null){
         arr =[]
     } else{
         arr.forEach(el =>{
             console.log(el)
-            formList.innerHTML += `<p>${el.activity}</p>`
+            formList.innerHTML += `
+            <div>
+                <b>${el.activity}</b>
+                <span> - <i class="material-icons">delete</i>
+                </span>
+            </div>`
         })
     }
 }
 
+const RemovefromList = (activity) => {
+    console.log(activity)
+    let arrIndex
+    arr.forEach((el, index) => {
+        console.log(index)
+        if(el.activity === activity){
+            arrIndex = index;
+        }
+    })
+    arr.splice(arrIndex,1)
+    SaveItem()
+}
+
 formEntry.addEventListener('submit', (e) => {
     e.preventDefault();  
-
+    
     let userActivity = document.querySelector('#form-activity').value
     console.log(userActivity)
-
+    
     CreateItem(userActivity)
     SaveItem();
     formEntry.reset();
 })
 
 document.addEventListener('DOMContentLoaded', PrintItem)
+
+formList.addEventListener('click', (e) => {
+    e.preventDefault();    
+    // console.log(e.target)
+    let deleteAction = e.target.innerHTML
+    deleteAction = 'done'
+    if(deleteAction === 'done'){
+        const listItem = e.path[1].childNodes[1].innerHTML
+        RemovefromList(listItem)
+    }
+    
+  })
